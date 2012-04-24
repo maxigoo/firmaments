421ee8d63720b6961ac67986451b3cb84beb036c
Patrick Bradley <patrickbradley777@gmail.com>
2012-04-23 20:53:11 -0400

    first zimpley commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zimpley::Application.config.secret_token = 'f31d29a824ac4b2c876e35b4f2a39c4067fc91347b69a53226c678ee0474c4efb901a817f7d7503c1b7594c73ec6b19ee467919314ad04bdc63d34a8a51f4e9b'
